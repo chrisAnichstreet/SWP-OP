9fa55f33c6fe22d3b41047b81e3e7be14b60586e
chrisAnichstreet <chrmanzl@tsn.at>
2020-04-07 23:07:33 +0200

    Einige spielerein und Übungen zu AJAX beim Secure Password projekt hinzugefügt.

---
+++ Secure_Password_Application/out/artifacts/Secure_Password_Application_war_exploded/ajaxHomePage.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    console.log("Test0");
+    $('#Portalgun')
+
+        console.log("Test1");
+        $('#bring').click(function(){
+            console.log("Hey Jetz kommt Ajax.");
+
+            /* "this" sprich der value (=die Kategorie) aus der JSP wird nicht als id Festgelegt, Überprüfen*/
+            //console.log($(this).html());
+
+            $.ajax({
+                url:'homepage',
+                type: 'POST',
+                dataType: 'json',
+                data: {value:$("openPortal").html()},
+                success: function(data)
+                {
+
+                    min = 0;
+                    max = 3;
+                    var rand = (Math.random() * (max - min)) + min;
+                    rand = Math.round(rand);
+
+                    console.log(rand);
+                    text = "<div style='text-align: center; font-family: Impact; font-weight: bold'>";
+
+                    text += '<a href="'+data.portal+'">';
+
+                    if(rand == 0)
+                    {
+                        text += '<IMG Src="https://media.giphy.com/media/yg5CaBHazViSY/giphy.gif">';
+                    }
+                    if(rand == 1)
+                    {
+                        text += '<IMG Src="https://media.giphy.com/media/11p6pFpPYIAzS0/giphy.gif">';
+                    }
+                    if(rand == 2)
+                    {
+                        text += '<IMG Src="https://media.giphy.com/media/cHgN1BeB8o836/giphy.gif">';
+                    }
+                    if(rand == 3)
+                    {
+                        text += '<IMG Src="https://media.giphy.com/media/HjeIqm3MxURFK/giphy.gif">';
+                    }
+                    text += '</a><p>To Infinity And Beyond!</p></div>';
+                    console.log("TEXT :"+text);
+                    $("#Portal").html(text);
+                }
+            });
+            return false;
+        });
+
+
+    });
--- Secure_Password_Application/src/main/java/htlinn/homepage.java
+++ Secure_Password_Application/src/main/java/htlinn/homepage.java
@@ -1,5 +1,7 @@
 package htlinn;
 
+import com.google.gson.Gson;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,11 +9,67 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 
 @WebServlet(name = "homepage")
 public class homepage extends HttpServlet {
+    Random rand = new Random();
+
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        System.out.println("Values im Parameter sollte so ausschauen "+((String)request.getParameter("value")));
+        response.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
+        Map<String,String> map = new HashMap<String,String>();
+        map.put("portal", openPortal());
+        flashOnPage(request,response, map);
+
+    }
+
+    private void flashOnPage(HttpServletRequest request, HttpServletResponse response, Map<String, String> map) throws IOException
+    {
+        response.setContentType("application/Json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(new Gson().toJson(map));
+
+    }
+
+
+    private String openPortal() {
+        String link = "https://www.google.com/maps/place/";
+        Random rand = new Random();
+        link = generateCoordinats(90,link);
+        if(rand.nextBoolean()){
+            link += 'N';
+        }
+        else {
+            link += 'S';
+        }
+        link += "+";
+        link = generateCoordinats(180,link);
+
+        if(rand.nextBoolean()){
+            link+= "E";
+        }
+        else{
+            link += "W";
+        }
+        return link;
+    }
+
+    private String generateCoordinats(int maxdegrees,String link) {
+
+        link += rand.nextInt(maxdegrees);
+        link += "°";
+        link += rand.nextInt(60);
+        link += "'";
+        link += rand.nextInt(60);
+        link += "''";
+
+        return link;
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
